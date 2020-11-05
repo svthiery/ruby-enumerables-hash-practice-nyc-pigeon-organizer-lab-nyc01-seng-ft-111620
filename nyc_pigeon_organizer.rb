dfa5775d820dca8683ec31271fe3b1b73517efad
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-11-05 22:27:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,46 @@
+require 'pry'
+
+def pigeon_names_getter(data)
+  pigeon_names = []
+  data.each do |key, value|
+    value.each do |attribute, names_array|
+      count = 0 
+      while count < names_array.length do
+        names_array.each {|name| pigeon_names << name}
+        count += 1 
+      end
+    end
+  end
+  pigeon_names = pigeon_names.uniq
+  pigeon_names
+end 
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_names_array = pigeon_names_getter(data)
+  pigeon_organizer = {}
+  pigeon_names_array.each do |element|
+    pigeon_organizer[element] = {:color => [], :gender => [], :lives => []}
+  end
+  data.each do |key, value|
+    value.each do |inner_key, inner_value|
+      inner_value.each do |name|
+        pigeon_organizer[name][key][inner_key]
+      end
+    end
+  end
+  pigeon_organizer
 end
+  
+#def nyc_pigeon_organizer(data)
+ # data.each_with_object({}) do |(key, value), final_array|
+ #   value.each do |inner_key, names|
+ #     names.each do |name|
+ #       if !final_array[name]
+ #         final_array[name] = {}
+ #       end
+ #     end
+ #   end
+ # end 
+#end
+
+
